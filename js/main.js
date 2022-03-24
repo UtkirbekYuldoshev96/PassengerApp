3a1382d3a47aa325169065e96b6745877affa2d4
UtkirbekYuldoshev96 <91976877+UtkirbekYuldoshev96@users.noreply.github.com>
2022-03-25 01:26:17 +0500

    CounterApp

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 let count = 0;
 let total = 0;
-
 // o'zgaruvchi yaratib olinadi
 const counter = document.getElementById('count');
 const incrimetn = document.getElementById('incriment');
@@ -9,7 +8,6 @@ const reset = document.getElementById('reset');
 const save_btn = document.getElementById('save-btn');
 const save_element = document.getElementById('save-element');
 const total_amount = document.getElementById('total-amount');
-
 // css klass chaqirib olindi
 const firstNameClass = document.querySelector('.firstNameClass');
 window.addEventListener('load', () => {
@@ -27,28 +25,75 @@ counter.addEventListener('mouseover', () => {
     counter.style.transition = "scale(1.2)";
     counter.style.opacity = 1;
 });
+counter.addEventListener('mouseout', () => {
+    counter.style.transition = "scale(1)";
+    counter.style.opacity = 0.6;
+});
+firstNameClass.addEventListener('mouseover', () => {
+    counter.style.transition = "scale(1.2)";
+    counter.style.opacity = 1;
+});
+firstNameClass.addEventListener('mouseout', () => {
+    counter.style.transition = "scale(1)";
+    counter.style.opacity = 0.6;
+});
+save_element.addEventListener('mouseover', () => {
+    counter.style.transition = "scale(1.2)";
+    counter.style.opacity = 1;
+});
+save_element.addEventListener('mouseout', () => {
+    counter.style.transition = "scale(1)";
+    counter.style.opacity = 0.6;
+});
+total_amount.addEventListener('mouseover', () => {
+    counter.style.transition = "scale(1.2)";
+    counter.style.opacity = 1;
+});
+total_amount.addEventListener('mouseout', () => {
+    counter.style.transition = "scale(1)";
+    counter.style.opacity = 0.6;
+});
+incrimetn.addEventListener('click', () => {
+    count += 1;
+    counter.innerHTML = count;
+    colorChange();
+});
+des.addEventListener('click', () => {
+    count -= 1;
+    counter.innerHTML = count;
+    colorChange();
+});
+reset.addEventListener("click", () => {
+    count = 0;
+    counter.innerHTML = count;
+    save_element.innerHTML = `Previous entries: ${count}`;
+    total = 0;
+    total_amount.textContent = `We have ${total} passengers`;
+    colorChange();
+});
 
-counter.addEventListener('mouseover', () => {
-    counter.style.transition = "scale(1.2)";
-    counter.style.opacity = 1;
+save_btn.addEventListener("click", () => {
+    total += count;
+    save_element.innerHTML = ` + ${count}`;
+    count = 0;
+    counter.innerHTML = count;
+        if (total >= 0){
+            total_amount.textContent = `We have ${total} passengers`;
+        }else{
+            total = 0;
+            total_amount.textContent = `We have ${total} passengers`
+        }
 });
-counter.addEventListener('mouseover', () => {
-    counter.style.transition = "scale(1.2)";
-    counter.style.opacity = 1;
-});
-counter.addEventListener('mouseover', () => {
-    counter.style.transition = "scale(1.2)";
-    counter.style.opacity = 1;
-});
-counter.addEventListener('mouseover', () => {
-    counter.style.transition = "scale(1.2)";
-    counter.style.opacity = 1;
-});
-counter.addEventListener('mouseover', () => {
-    counter.style.transition = "scale(1.2)";
-    counter.style.opacity = 1;
-});
-counter.addEventListener('mouseover', () => {
-    counter.style.transition = "scale(1.2)";
-    counter.style.opacity = 1;
-});
+
+const colorChange = () => {
+  if (count > 0){
+      counter.style.color = "green";
+      firstNameClass.innerHTML = "People entered:";
+  }else if (count < 0){
+      counter.style.color = "red";
+      firstNameClass.innerHTML = "People entered:";
+  }else {
+      counter.style.color = "wheat";
+      firstNameClass.innerHTML = "People entered:";
+  }
+};
