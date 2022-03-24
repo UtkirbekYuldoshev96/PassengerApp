e1d65e3c8cc477db825e146a0a25539be00ba8a6
UtkirbekYuldoshev96 <91976877+UtkirbekYuldoshev96@users.noreply.github.com>
2022-03-24 23:53:03 +0500

    PassengerApp

---
+++ js/main.js
@@ -0,0 +1,54 @@
+let count = 0;
+let total = 0;
+
+// o'zgaruvchi yaratib olinadi
+const counter = document.getElementById('count');
+const incrimetn = document.getElementById('incriment');
+const des = document.getElementById('decres');
+const reset = document.getElementById('reset');
+const save_btn = document.getElementById('save-btn');
+const save_element = document.getElementById('save-element');
+const total_amount = document.getElementById('total-amount');
+
+// css klass chaqirib olindi
+const firstNameClass = document.querySelector('.firstNameClass');
+window.addEventListener('load', () => {
+    counter.style.transition = "transform 0.7s linear";
+    counter.style.opacity = 0.6;
+    firstNameClass.style.transition = "transform 0.5s linear";
+    firstNameClass.style.opacity = 0.6;
+    save_element.style.transition = "transform 0.5s linear";
+    save_element.style.opacity = 0.6;
+    total_amount.style.transition = "transform 0.5s linear";
+    total_amount.style.opacity = 0.6;
+});
+
+counter.addEventListener('mouseover', () => {
+    counter.style.transition = "scale(1.2)";
+    counter.style.opacity = 1;
+});
+
+counter.addEventListener('mouseover', () => {
+    counter.style.transition = "scale(1.2)";
+    counter.style.opacity = 1;
+});
+counter.addEventListener('mouseover', () => {
+    counter.style.transition = "scale(1.2)";
+    counter.style.opacity = 1;
+});
+counter.addEventListener('mouseover', () => {
+    counter.style.transition = "scale(1.2)";
+    counter.style.opacity = 1;
+});
+counter.addEventListener('mouseover', () => {
+    counter.style.transition = "scale(1.2)";
+    counter.style.opacity = 1;
+});
+counter.addEventListener('mouseover', () => {
+    counter.style.transition = "scale(1.2)";
+    counter.style.opacity = 1;
+});
+counter.addEventListener('mouseover', () => {
+    counter.style.transition = "scale(1.2)";
+    counter.style.opacity = 1;
+});
